9954b89feeb28937b3e3cd1c0c526b1c1c6d730c
caseLearner0718 <105021025@live.asia.edu.tw>
2016-10-25 23:54:03 +0800

    Update hw05.java

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -7,11 +7,33 @@ public class hw05 {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		int[][] val = new int[n][n];
-		int b=0,c=0,d=0;
+		int x=0,y=0,where=0,where2=0;
+		boolean flat = false;
 		for(int a=1;a<=n*n;a++){
-			
-			val[b][c]=a;
+			val[y][x]=a;
+//			System.out.println("目前位置X:" + (x+1) + " Y:" + (y+1) + " a:" + a);
+			if(y==0 && flat==false){
+				where++;
+				x=0;
+				y=where;
+			}else if(x==n-1 && flat==true){
+				where2++;
+				y=n-1;
+				x=where2;
+			}else{
+				x++;
+				y--;
+			}
+			if(y==0&&x==n-1){
+				flat=true;
+			}
+		}
+		
+		for(int a=0;a<n;a++){
+			for(int b=0;b<n;b++){
+				System.out.print(val[a][b] + "\t");
+			}
+			System.out.println();
 		}
 	}
-
 }
